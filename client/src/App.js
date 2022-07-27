d37ce4fe879c9662bf3385b01d336f1f31de01c3
Jose Pardo <josmar.pm19@gmail.com>
2022-07-26 19:22:19 -0500

    front advance

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 function App() {
+
+  const [ state, setState ] = useState('')
+  const [ stateItem, setStateItem ] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [ setStateItem, state ])
+
+  const getData = async() => {
+    const resp = await axios.get('http://localhost:4444/iecho/all')
+    setStateItem( resp.data )
+  }
+
+  const handleSubmit = async( e ) => {
+    try {
+      e.preventDefault();
+      await axios.get(`http://localhost:4444/iecho?text=${ state }`)
+      setState('')
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+  console.log(stateItem)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <form onSubmit={ handleSubmit }>
+          <input 
+            type="text"
+            value={ state }
+            placeholder='Enter a new text'
+            onChange={e => setState(e.target.value)}
+          />
+          <button type='submit'>send</button>
+        </form>
+      </div>
+
+    </>
   );
 }
 
